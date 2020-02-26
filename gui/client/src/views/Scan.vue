a6bee3c2406a16aacc73725203acde0b3ec5a44e
sundowndev <raphael@crvx.fr>
2020-02-26 22:54:24 +0100

    refactor(client): scanner implementations

--- gui/client/src/views/Scan.vue
+++ gui/client/src/views/Scan.vue
@@ -9,101 +9,87 @@
       >
         <b-form-input
           id="input-number"
-          v-model="form.number"
+          v-model="number"
           type="text"
           required
           placeholder="e.g: 33678132393"
+          :disabled="loading"
         ></b-form-input>
       </b-form-group>
 
-      <div>
-        <b-button
-          variant="outline-primary"
-          v-for="scanner in scanners"
-          v-bind:key="scanner.name"
-          v-on:click="runScan(scanner)"
-        >{{ scanner.name }}</b-button>
-        <b-button variant="danger" v-on:click="clear">Clear results</b-button>
-      </div>
+      <b-button size="sm" variant="dark" v-on:click="runScans" :disabled="loading">
+        <b-icon-play-fill></b-icon-play-fill>Run scan
+      </b-button>
+
+      <b-button variant="light" size="sm" v-on:click="clearData">Clear results</b-button>
     </b-form>
 
     <hr />
 
-    <div
-      v-for="scanner in scanners"
-      v-bind:key="scanner.name"
-      :id="scanner.id"
-      v-show="scanner.loading || scanner.data.length > 0"
-    >
-      <h3>{{ scanner.name }}</h3>
-      <i v-show="scanner.loading">Running scan for {{ form.number }}...</i>
-      <b-table
-        outlined
-        :stacked="scanner.data.length == 1"
-        :items="scanner.data"
-        v-show="scanner.data.length > 0"
-      ></b-table>
-    </div>
-
-    <GoogleSearch/>
+    <LocalScan />
+    <NumverifyScan />
+    <GoogleSearch />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { mapMutations } from "vuex";
+import LocalScan from "../components/LocalScan.vue";
+import NumverifyScan from "../components/NumverifyScan.vue";
 import GoogleSearch from "../components/GoogleSearch.vue";
 
 interface Scanner {
   id: string;
   name: string;
-  data: unknown[];
+  data: object[];
   loading: boolean;
 }
 
-interface ScanData {
-  form: {
-    number: string;
-  };
-  scanners: Scanner[];
+interface Data {
+  loading: boolean;
+  number: string;
 }
 
 export default Vue.extend({
-  components: { GoogleSearch },
+  components: { LocalScan, GoogleSearch, NumverifyScan },
   computed: {
     ...mapMutations(["pushError"])
   },
-  data(): ScanData {
+  data(): Data {
     return {
-      form: {
-        number: ""
-      },
-      scanners: [
-        { id: "local", name: "Local scan", data: [], loading: false },
-        { id: "numverify", name: "Numverify", data: [], loading: false },
-        { id: "googlesearch", name: "Google search", data: [], loading: false }
-      ]
+      loading: false,
+      number: ""
     };
   },
   methods: {
-    clear() {
-      for (const scanner of this.scanners) {
-        scanner.loading = false;
-        scanner.data = [];
-      }
+    clearData() {
+      console.log("clear data");
     },
-    async runScan(scanner: Scanner): Promise<void> {
-      if (this.form.number.length < 2) {
+    // async runScan(scanner: Scanner): Promise<void> {
+    //   if (this.number.length < 2) {
+    //     this.$store.commit("pushError", { message: "Number is not valid." });
+    //     return;
+    //   }
+
+    //   scanner.loading = true;
+    //   this.$store.commit("setNumber", this.number);
+
+    //   const res = await this.$store.dispatch("runScanner", scanner.id);
+    //   scanner.data.push(res.result);
+    //   scanner.loading = false;
+    // },
+    async runScans(scanner: Scanner): Promise<void> {
+      if (this.number.length < 2) {
         this.$store.commit("pushError", { message: "Number is not valid." });
         return;
       }
 
-      scanner.loading = true;
-      this.$store.commit("setNumber", this.form.number);
+      this.loading = true;
 
-      const res = await this.$store.dispatch("runScanner", scanner.id);
-      scanner.data.push(res.result);
-      scanner.loading = false;
+      this.$store.commit("setNumber", this.number);
+
+      this.loading = false;
     },
     onSubmit(evt: any) {
       evt.preventDefault();
