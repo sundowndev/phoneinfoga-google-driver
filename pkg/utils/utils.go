9b0cf7d46caa81033026f7e2658544dc1e47f82b
sundowndev <raphael@crvx.fr>
2020-02-29 22:49:02 +0100

    test: utils

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,36 +1,34 @@
 package utils
 
 import (
-	"log"
 	"os"
 	"regexp"
-	"strconv"
 	"strings"
 
 	phoneiso3166 "github.com/onlinecity/go-phone-iso3166"
 )
 
-// FormatNumber ...
-func FormatNumber(n string) (num string) {
-	num = strings.Replace(n, "+", "", 1)
+// FormatNumber formats a phone number to remove
+// unnecessary chars and avoid dealing with unwanted input.
+func FormatNumber(n string) string {
+	n = strings.ReplaceAll(n, "+", "")
+	n = strings.ReplaceAll(n, "-", "")
+	n = strings.ReplaceAll(n, "(", "")
+	n = strings.ReplaceAll(n, ")", "")
+	n = strings.ReplaceAll(n, " ", "")
 
 	re := regexp.MustCompile("^[0-9]+$")
 
-	if len(re.FindString(num)) == 0 {
+	if len(re.FindString(n)) == 0 {
 		LoggerService.Errorln("Number is not valid.")
 		os.Exit(0)
 	}
 
-	return num
+	return n
 }
 
-// ParseCountryCode ...
+// ParseCountryCode parses a phone number and returns ISO country code.
+// This is required in order to use the phonenumbers library.
 func ParseCountryCode(n string) string {
-	num, err := strconv.ParseUint(FormatNumber(n), 10, 64)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return phoneiso3166.E164.Lookup(num)
+	return phoneiso3166.E164.LookupString(FormatNumber(n))
 }
