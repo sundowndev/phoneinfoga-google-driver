21383805645c00ed5db9de89fdeac44853426b0f
sundowndev <raphael@crvx.fr>
2020-04-26 17:13:26 +0100

    feat: init scanner usage

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sundowndev/phoneinfoga-google-driver
 
 go 1.13
+
+require gopkg.in/sundowndev/phoneinfoga.v2 v2.0.8 // indirect
--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+    "fmt"
+    "log"
+
+    phoneinfoga "gopkg.in/sundowndev/phoneinfoga.v2/pkg/scanners"
+)
+
+func main() {
+    number, err := phoneinfoga.LocalScan("<number>")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    links := phoneinfoga.GoogleSearchScan(number)
+
+    for _, link := range links.Individuals {
+        fmt.Println(link.URL) // Google search link to scan
+    }
+}
+
